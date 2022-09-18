42266d7fe005cdd0b7102ec4530358d4facb6872
Ulyanov Nikita <uliyanov87@icloud.com>
2022-09-18 20:29:55 +0300

    it just works

--- TrafficLights/ViewController.swift
+++ TrafficLights/ViewController.swift
@@ -16,11 +16,27 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        switchButton.layer.cornerRadius = 10
+        redLight.layer.cornerRadius = 38
+        yellowLight.layer.cornerRadius = 38
+        greenLight.layer.cornerRadius = 38
     }
-
+    
     @IBAction func switchButtonDidTapped() {
+        switchButton.setTitle("NEXT", for: .normal)
+        
+        if redLight.alpha == 1, yellowLight.alpha.isLess(than: 1), greenLight.alpha.isLess(than: 1) {
+            redLight.alpha = 0.3
+            yellowLight.alpha = 1
+        } else if redLight.alpha.isLess(than: 1), yellowLight.alpha == 1, greenLight.alpha.isLess(than: 1) {
+            yellowLight.alpha = 0.3
+            greenLight.alpha = 1
+        } else {
+            greenLight.alpha = 0.3
+            redLight.alpha = 1
+        }
     }
+    
     
 }
 
