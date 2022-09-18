aedc19fc7cad9e37f4b5607cafeca63bb8c743e6
Ulyanov Nikita <uliyanov87@icloud.com>
2022-09-18 17:48:02 +0300

    Final setup UI

--- TrafficLights/ViewController.swift
+++ TrafficLights/ViewController.swift
@@ -8,17 +8,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet var switchButton: UIButton!
     
     @IBOutlet var redLight: UIView!
-    
     @IBOutlet var yellowLight: UIView!
-    
     @IBOutlet var greenLight: UIView!
+    @IBOutlet var switchButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        switchButton.layer.cornerRadius = 10
         
     }
 
